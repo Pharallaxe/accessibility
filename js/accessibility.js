370881238df59946ddbfff7713459ef9fe387d67
Pharallaxe <103119043+Pharallaxe@users.noreply.github.com>
2024-06-30 16:03:19 +0200

    Ajout du LocalStorage.

--- js/accessibility.js
+++ js/accessibility.js
@@ -2,6 +2,12 @@ function $(selector) {
     return document.querySelector(selector);
 }
 
+/*********************************************************************************
+ * PARTIE CONSTANTE
+ *********************************************************************************/
+
+const accessibilityPanel = $("#accessibilityPanel");
+
 
 class Button {
     static fontSizeDecrease = $("#fontSizeDecreaseButton");
@@ -24,27 +30,61 @@ class Property {
     static wordSpacing = "--word-spacing";
 }
 
+/*********************************************************************************
+ * PARTIE CHARGEMENT
+ *********************************************************************************/
+
+function readAccessibilitySettings() {
+    const accessibilitySettings = JSON.parse(localStorage.getItem('accessibility')) || {};
+
+    // Mettre à jour les variables CSS avec les valeurs enregistrées
+    for (const [property, value] of Object.entries(accessibilitySettings)) {
+        document.documentElement.style.setProperty(property, value);
+    }
+}
+
+
+/*********************************************************************************
+ * PARTIE ENREGISTREMENT
+ *********************************************************************************/
 
 /**
- * Ajuste la valeur d'une propriété CSS sur l'élément racine (root).
- * @param {string} property - La propriété CSS à ajuster.
- * @param {number} adjustment - La valeur d'ajustement à ajouter à la propriété CSS.
- * @param {string} [unit='px'] - L'unité de la valeur d'ajustement (par défaut : 'px').
+ * Enregistre les paramètres d'accessibilité dans le localStorage.
+ * @param {string} cssProperty - La propriété CSS à enregistrer.
+ * @param {string} cssPropertyValue - La valeur de la propriété CSS à enregistrer.
  */
-function adjustRoot(property, adjustment, unit = 'px') {
-    console.log(property, adjustment, unit);
-    const root = document.documentElement;
-    root.style.setProperty(property, `${parseFloat(getComputedStyle(root).getPropertyValue(property)) + adjustment}${unit}`);
+function saveAccessibilitySettings(cssProperty, cssPropertyValue) {
+    const existingAccessibilitySettings = JSON.parse(localStorage.getItem('accessibility')) || {};
+    existingAccessibilitySettings[cssProperty] = cssPropertyValue;
+    localStorage.setItem('accessibility', JSON.stringify(existingAccessibilitySettings));
 }
 
 /**
- * Ajoute un écouteur d'événement 'click' à un élément et ajuste une propriété CSS sur l'élément racine (root) lors du clic.
+ * Ajuste la valeur d'une propriété CSS sur l'élément racine (root) et enregistre les
+ * paramètres d'accessibilitédans le localStorage.
+ * @param {string} cssProperty - La propriété CSS à ajuster.
+ * @param {number} adjustmentValue - La valeur d'ajustement à ajouter à la propriété CSS.
+ * @param {string} [unit='px'] - L'unité de la valeur d'ajustement (par défaut : 'px').
+ */
+function adjustRoot(cssProperty, adjustmentValue, unit = 'px') {
+    console.log(cssProperty, adjustmentValue, unit);
+    const rootElement = document.documentElement;
+    const currentRootValue = parseFloat(getComputedStyle(rootElement).getPropertyValue(cssProperty));
+    const newPropertyValue = `${currentRootValue + adjustmentValue}${unit}`;
+    rootElement.style.setProperty(cssProperty, newPropertyValue);
+    saveAccessibilitySettings(cssProperty, newPropertyValue);
+}
+
+/**
+ * Ajoute un écouteur d'événement 'click' à un élément et ajuste une propriété CSS sur
+ * l'élément racine (root) lors du clic.
  * @param {HTMLElement} element - L'élément auquel ajouter l'écouteur d'événement.
  * @param {string} property - La propriété CSS à ajuster sur l'élément racine.
  * @param {number} value - La valeur d'ajustement à ajouter à la propriété CSS.
  * @param {string} [unit='px'] - L'unité de la valeur d'ajustement (par défaut : 'px').
  */
-function addEventButton(element, property, value, unit = 'px') {
+function addEventButton(element, property, value, unit = 'px'
+) {
     element.addEventListener('click', () => adjustRoot(property, value, unit));
 }
 
@@ -54,7 +94,7 @@ function addEventButton(element, property, value, unit = 'px') {
  */
 function initializeFontSize() {
     addEventButton(Button.fontSizeDecrease, Property.fontSize, -2);
-    addEventButton(Button.fontSizeIncrease, Property.fontSize,  2);
+    addEventButton(Button.fontSizeIncrease, Property.fontSize, 2);
 }
 
 /**
@@ -63,7 +103,7 @@ function initializeFontSize() {
  */
 function initializeLineHeight() {
     addEventButton(Button.lineHeightDecrease, Property.lineHeight, -0.1, "");
-    addEventButton(Button.lineHeightIncrease, Property.lineHeight,  0.1, "");
+    addEventButton(Button.lineHeightIncrease, Property.lineHeight, 0.1, "");
 }
 
 /**
@@ -72,7 +112,7 @@ function initializeLineHeight() {
  */
 function initializeLetterSpacing() {
     addEventButton(Button.letterSpacingDecrease, Property.letterSpacing, -0.05, 'em');
-    addEventButton(Button.letterSpacingIncrease, Property.letterSpacing,  0.05, 'em');
+    addEventButton(Button.letterSpacingIncrease, Property.letterSpacing, 0.05, 'em');
 }
 
 /**
@@ -81,11 +121,18 @@ function initializeLetterSpacing() {
  */
 function initializeWordSpacing() {
     addEventButton(Button.wordSpacingDecrease, Property.wordSpacing, -0.1, 'em');
-    addEventButton(Button.wordSpacingIncrease, Property.wordSpacing,  0.1, 'em');
+    addEventButton(Button.wordSpacingIncrease, Property.wordSpacing, 0.1, 'em');
+}
+
+if (accessibilityPanel) {
+    initializeFontSize();
+    initializeLetterSpacing();
+    initializeLineHeight();
+    initializeWordSpacing();
 }
 
 
-initializeFontSize();
-initializeLetterSpacing();
-initializeLineHeight();
-initializeWordSpacing();
+readAccessibilitySettings();
+
+
+
